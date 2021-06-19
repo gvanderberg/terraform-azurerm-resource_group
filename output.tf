c4fb502f01a62b8839587e11e1dcab3a801a41a5
Gareth van der Berg <gareth.shaun@gmail.com>
2021-06-19 18:03:59 +0200

    update output

--- output.tf
+++ output.tf
@@ -1,11 +1,3 @@
-output "id" {
-  value = element(coalescelist(azurerm_resource_group.this.*.id, data.azurerm_resource_group.this.*.id), 0)
-}
-
-output "name" {
-  value = element(coalescelist(azurerm_resource_group.this.*.name, data.azurerm_resource_group.this.*.name), 0)
-}
-
-output "location" {
-  value = element(coalescelist(azurerm_resource_group.this.*.location, data.azurerm_resource_group.this.*.location), 0)
+output "this" {
+  value = element(coalescelist(azurerm_resource_group.this.*, data.azurerm_resource_group.this.*), 0)
 }
